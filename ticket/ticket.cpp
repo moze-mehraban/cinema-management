1f67816d5940e5e1c6cd9ec1853a66d3092711cf
ahmad hosseini <seyedahmad1382@gmail.com>
2024-01-06 20:52:46 +0330

    add film stuff

--- ticket/ticket.cpp
+++ ticket/ticket.cpp
@@ -1,6 +1,107 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+class Film {
+private:
+	int ID;
+	char name[30];
+public:
+	Film();
+	Film(int ID);
+	void setName(char[]);
+	void setID(int);
+	char* getName();
+	int getID();
+	void Print();
+};
+char* Film::getName() {
+	return name;
+}
+int Film::getID() {
+	return ID;
+}
+void Film::Print() {
+	cout << ID << "\t" << name << endl;
+}
+Film::Film() {
+
+}
+void Film::setID(int id) {
+	this->ID = id;
+}
+Film::Film(int ID) {
+	this->ID = ID;
+}
+void Film::setName(char n[]) {
+	int i = 0;
+	for (i = 0; n[i] != '\0'; i++)
+		name[i] = n[i];
+	name[i] = '\0';
+}
+class AllFilms {
+private:
+	Film* films;
+	int Capacity;
+	int numFilm;
+public:
+	AllFilms(int capacity);
+	void Add(Film);
+	void remove(int);
+	void ReadFromFile();
+	void WriteToFile();
+	void Print();
+};
+void AllFilms::WriteToFile() {
+	ofstream out;
+	out.open("film.txt");
+	for (int i = 0; i < numFilm; i++) {
+		out << films[i].getID() << "\t" << films[i].getName()
+			<< endl;
+	}
+	out.close();
+}
+void AllFilms::Print() {
+	for (int i = 0; i < numFilm; i++)
+	{
+		films[i].Print();
+	}
+}
+AllFilms::AllFilms(int capacity) {
+	this->Capacity = capacity;
+	numFilm = 0;
+	films = new Film[capacity];
+}
+void AllFilms::Add(Film f) {
+	if (numFilm < Capacity) {
+		films[numFilm] = f;
+		numFilm++;
+	}
+}
+void AllFilms::remove(int) {
+
+}
+void AllFilms::ReadFromFile() {
+	int ID;
+	char filmName[20];
+	ifstream inputFile;
+	inputFile.open("film.txt");
+	if (inputFile.is_open() == false) {
+		cout << "film is not open \n";
+	}
+	else
+	{
+		cout << "film is open \n";
+		while (inputFile.eof() == false) {
+			inputFile >> ID;
+			inputFile >> filmName;
+			Film film(ID);
+			film.setName(filmName);
+			Add(film);
+		}
+	}
+
+	inputFile.close();
+}
 class ticket {
 private:
 	string name;
